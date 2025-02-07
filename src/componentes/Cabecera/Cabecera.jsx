54a8c0f66f8c85bc4f9e3ce407eead605960adda
MarioAviles <4150123@alu.murciaeduca.es>
2025-02-07 13:40:46 +0100

    He añadido las banderas sin poner bien el css y cambiado el fondo

--- src/componentes/Cabecera/Cabecera.jsx
+++ src/componentes/Cabecera/Cabecera.jsx
@@ -1,4 +1,6 @@
 import './Cabecera.css'
+import banderaEspaña from '../../assets/banderaEspaña.svg'
+import banderaInglaterra from '../../assets/banderaInglaterra.svg'
 
 const Cabecera = () => {
     return (
@@ -6,10 +8,13 @@ const Cabecera = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        {/* <img src="" alt="" />  LOGO */}
-                        <h1>Marca Personal FP</h1>
-                        {/* <img src="" alt="" />
-                        <img src="" alt="" />   BANDERAS DE IDIOMAS*/}
+                        <div className="header">
+                            <img className="bandera"src={banderaEspaña} alt="" />
+                            <div className="titulos">
+                                <h1 >Marca Personal FP</h1>
+                            </div>
+                            <img className="bandera"src={banderaInglaterra} alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
