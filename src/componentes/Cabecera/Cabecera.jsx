8d2e95d69723a8cf4256f5976ca820ce3d79e181
MarioAviles <4150123@alu.murciaeduca.es>
2025-02-07 13:02:05 +0100

    Preparación cabecera

---
+++ src/componentes/Cabecera/Cabecera.jsx
@@ -0,0 +1,13 @@
+import './Cabecera.css'
+
+const Cabecera = () => {
+    return (
+        <nav>
+            {/* <img src="" alt="" />  LOGO */}
+            <h1>Marca Personal FP</h1>
+            {/* <img src="" alt="" />
+            <img src="" alt="" />   BANDERAS DE IDIOMAS*/}
+        </nav>
+    )
+}
+export default Cabecera;
