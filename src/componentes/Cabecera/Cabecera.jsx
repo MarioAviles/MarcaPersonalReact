d0da8b52519053127a29bc7ff3e04b0c375925a9
MarioAviles <4150123@alu.murciaeduca.es>
2025-02-07 13:11:01 +0100

    Implementacion de bootstrap en el index.html

--- src/componentes/Cabecera/Cabecera.jsx
+++ src/componentes/Cabecera/Cabecera.jsx
@@ -3,10 +3,16 @@ import './Cabecera.css'
 const Cabecera = () => {
     return (
         <nav>
-            {/* <img src="" alt="" />  LOGO */}
-            <h1>Marca Personal FP</h1>
-            {/* <img src="" alt="" />
-            <img src="" alt="" />   BANDERAS DE IDIOMAS*/}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        {/* <img src="" alt="" />  LOGO */}
+                        <h1>Marca Personal FP</h1>
+                        {/* <img src="" alt="" />
+                        <img src="" alt="" />   BANDERAS DE IDIOMAS*/}
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
